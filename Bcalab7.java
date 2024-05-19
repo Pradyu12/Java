fa090808794596253f55fdc355c4fa7a81529e42
Pradyu12 <115913218+Pradyu12@users.noreply.github.com>
2024-05-19 14:39:14 +0530

    Add files via upload

---
+++ Bcalab7.java
@@ -0,0 +1,28 @@
+package assign;
+import java.util.Scanner;
+public class Primeornot {
+	public static void main(String[] args) {
+		   int j,num,flag=0;
+		   System.out.println("Enter the number:");
+		   Scanner s=new Scanner(System.in);
+		   num=s.nextInt();
+		   for(j=2;j<num;j++)
+           if(num% j==0)
+           {
+        	   flag=0;
+        	   break;}
+        	   else {
+        		   flag=1;
+        	   }
+ 
+		   if(flag==1)
+		   {
+			   System.out.println(num+" is a prime number");
+		   }
+		   else
+		   {
+			   System.out.println(num+" is not a prime number");
+		   }
+	}
+
+}
