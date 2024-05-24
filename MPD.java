4f8ae569cf3faef36c1a257de21e7d60ec13406c
Pradyumna SK <115913218+Pradyu12@users.noreply.github.com>
2024-05-24 10:12:28 +0530

    Add files via upload

---
+++ MPD.java
@@ -0,0 +1,33 @@
+package Menupulldown;
+import java.awt.*;
+public class Menupulldown 
+{
+Menupulldown()
+{
+	java.awt.Frame f=new java.awt.Frame("Menu and Menu Item Example");
+	MenuBar mb=new MenuBar();
+	Menu menu=new Menu("Menu");
+	Menu submenu=new Menu("Sub Menu");
+	MenuItem i1=new MenuItem("Item 1");
+	MenuItem i2=new MenuItem("Item 2");
+	MenuItem i3=new MenuItem("Item 3");
+	MenuItem i4=new MenuItem("Item 4");
+	MenuItem i5=new MenuItem("Item 5");
+	menu.add(i1);
+	menu.add(i2);
+	menu.add(i3);
+	submenu.add(i4);
+	submenu.add(i5);
+	menu.add(submenu);
+	mb.add(menu);
+	f.setMenuBar(mb);
+	f.setSize(400, 400);
+	f.setLayout(null);
+	f.setVisible(true);
+}
+	public static void main(String[] args) {
+		new Menupulldown();
+
+	}
+
+}
